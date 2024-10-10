ccf3f700e0d28e2a0fcbb62a3abfa26524096194
dajinYang <didekwls0104@naver.com>
2024-10-10 23:48:53 +0900

    1주차(필수) : 일기 조회, 작성, 수정, 삭제 기능 구현

---
+++ src/main/java/org/sopt/seminar1/DiaryService.java
@@ -0,0 +1,44 @@
+package org.sopt.seminar1;
+
+import org.sopt.seminar1.DiaryRepository;
+
+import java.util.List;
+
+public class DiaryService {
+    private final DiaryRepository diaryRepository = new DiaryRepository();
+
+    // 일기 작성
+    void writeDiary(final String body) {
+        checkLength(body);
+
+        final Diary diary = new Diary(null, body);
+        diaryRepository.save(diary);
+    }
+
+    // 일기 조회
+    List<Diary> getDiaryList() {
+        return diaryRepository.findAll();
+    }
+
+    // 일기 수정
+    void patchDiary(final String id, final String body) {
+        Long longId = Converter.parseId(id);
+
+        checkLength(body);
+
+        diaryRepository.patch(longId, body);
+    }
+
+    // 일기 삭제
+    void deleteDiary(final String id) {
+        Long longId = Converter.parseId(id);
+
+        diaryRepository.delete(longId);
+    }
+
+    private void checkLength(final String body) {
+        if (body.length() > 30) {
+            throw new IllegalArgumentException("30글자 초과");
+        }
+    }
+}
