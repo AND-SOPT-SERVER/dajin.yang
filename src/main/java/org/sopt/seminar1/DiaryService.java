eecba9dacad6f6d46ef3760ac0d3c29bbaf90939
dajinYang <didekwls0104@naver.com>
2024-10-11 19:04:52 +0900

    1주차(선택) : 일기 수정 하루에 2번만 가능하도록 하는 기능 구현

--- src/main/java/org/sopt/seminar1/DiaryService.java
+++ src/main/java/org/sopt/seminar1/DiaryService.java
@@ -2,17 +2,25 @@ package org.sopt.seminar1;
 
 import org.sopt.seminar1.DiaryRepository;
 
+import java.time.LocalDate;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class DiaryService {
     private final DiaryRepository diaryRepository = new DiaryRepository();
+    private final Map<Long, Map<LocalDate, Integer>> count = new HashMap<>();
+
 
     // 일기 작성
     void writeDiary(final String body) {
+
         checkLength(body);
 
         final Diary diary = new Diary(null, body);
         diaryRepository.save(diary);
+
+        count.put(diary.getId(), new HashMap<>());
     }
 
     // 일기 조회
@@ -23,16 +31,24 @@ public class DiaryService {
     // 일기 수정
     void patchDiary(final String id, final String body) {
         Long longId = Converter.parseId(id);
-
         checkLength(body);
 
+        LocalDate today = LocalDate.now();
+        Map<LocalDate, Integer> diaryCnt = count.getOrDefault(longId, new HashMap<>());
+        int todayCnt = diaryCnt.getOrDefault(today, 0);
+
+        if (todayCnt >= 2) {
+            throw new IllegalStateException("일기 수정을 하루에 2번만 가능합니다");
+        }
+
         diaryRepository.patch(longId, body);
+        diaryCnt.put(today, todayCnt + 1);
+        count.put(longId, diaryCnt);
     }
 
     // 일기 삭제
     void deleteDiary(final String id) {
         Long longId = Converter.parseId(id);
-
         diaryRepository.delete(longId);
     }
 
